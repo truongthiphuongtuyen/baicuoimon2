2aa2f8a11a83c1f0f13dd04a43eedf9f5d9897b4
truongthiphuongtuyen <tuyen.ttp124@arena-multimedia.edu.vn>
2025-05-05 03:36:46 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+strings: ["Web Designer.", "Code Developer.","Apps Designer." , "Film Maker."],
+typeSpeed:50,
+backSpeed:50,
+loop:true,
+});
+
+$('.timeline-panel').click(function() {
+    $('.timeline-body', this).toggle(); // p00f
+});
+
+$('.counter').countUp();
+
+  AOS.init();
